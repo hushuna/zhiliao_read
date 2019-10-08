85a3a8dfb039ea7d445f084a9949b8f944c14399
hushuna <19945096907@136.com>
2019-10-08 14:27:06 +0800

    faf

---
+++ src/main/java/com/zhiliao/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.zhiliao.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: hsn
+ * @description:
+ * @date: 2019/10/8 11:14
+ **/
+@RestController
+public class TestController {
+
+    private Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @RequestMapping(value = "/test")
+    public String test(){
+        return "hello";
+    }
+}
---
+++ src/main/java/com/zhiliao/dao/TestDao.java
@@ -0,0 +1,13 @@
+package com.zhiliao.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author: hsn
+ * @description:
+ * @date: 2019/10/8 12:04
+ **/
+@Mapper
+public interface TestDao {
+
+}
